1acb3809e108cb1e6082a7be858748afa8b141e7
cailliaud <cailliaud.a@gmail.com>
2020-07-10 14:48:00 +0200

    [Feature] finalize output for orders listing

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,10 +2,27 @@ import React from 'react';
 import classes from "./Order.module.css";
 
 const Order = (props) => {
+
+    const ingredients= [];
+    for(let ingredientName in props.ingredients){
+        ingredients.push({
+            name: ingredientName,
+            amount:props.ingredients[ingredientName]
+        });
+    }
+
+    const ingredientOutput= ingredients.map(
+        ingredient =>{
+            return <span
+                className={classes.Ingredient}
+                key={ingredient.name}>{ingredient.name} ({ingredient.amount})</span>
+        }
+    );
+
     return (
         <div className={classes.Order}>
-            <p>Ingredients: Salad (1)</p>
-            <p>Price: <strong>5.00€</strong></p>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>{Number.parseFloat(props.price).toFixed(2)}€</strong></p>
         </div>
     );
 };
