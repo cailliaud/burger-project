a8589232ab8715e834718063a5ecad689dde460f
cailliaud <cailliaud.a@gmail.com>
2020-07-13 13:52:10 +0200

    [Redux] add redux to project (no asyncmanagement for the moment)

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -2,57 +2,38 @@ import React, {Component} from 'react';
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
 import {Route} from "react-router-dom";
 import ContactData from "./ContactData/ContactData";
+import {connect} from "react-redux";
 
 class Checkout extends Component {
 
-    state = {
-        ingredients: {
-            salad: 1,
-            meat: 1,
-            cheese: 2,
-            bacon: 1
-        },
-        totalPrice: 0
-    }
-
-    componentDidMount() {
-        const query = new URLSearchParams(this.props.location.search);
-        const ingredients= {};
-        let price = 0;
-        for(let param of query.entries()){
-            if(param[0] === 'price'){
-                price = param[1];
-            }else{
-                ingredients[param[0]]= +param[1];
-            }
-        }
-        this.setState({
-            ingredients: ingredients,
-            totalPrice: price
-        })
-    }
-
-    onCheckoutCancelled= () =>{
+    onCheckoutCancelled = () => {
         this.props.history.goBack();
     }
 
-    onCheckoutContinued= () => {
-        this.props.history.push(this.props.match.path+'/contact-data');
+    onCheckoutContinued = () => {
+        this.props.history.push(this.props.match.path + '/contact-data');
     }
+
     render() {
         return (
             <div>
                 <CheckoutSummary
-                    ingredients={this.state.ingredients}
+                    ingredients={this.props.ingredients}
                     onCheckoutCancelled={this.onCheckoutCancelled}
                     onCheckoutContinued={this.onCheckoutContinued}
                 />
-                <Route path={this.props.match.path + '/contact-data'} render={(props) => (
-                    <ContactData {...props} ingredients={this.state.ingredients} price={this.state.totalPrice}/>
-                )}/>
+                <Route path={this.props.match.path + '/contact-data'}
+                    component={ContactData}/>
             </div>
         );
     }
 }
 
-export default Checkout;
+const mapStateToProps = state => {
+    return {
+        ingredients: state.ingredients,
+        price: state.totalPrice
+    }
+}
+
+export default connect(mapStateToProps)(Checkout);
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import classes from "./ContactData.module.css";
 import axios from "./../../../config/axios-orders";
 import Spinner from "../../../components/UI/Spinner/Spinner";
 import Input from "../../../components/UI/Input/Input";
+import {connect} from "react-redux";
 
 class ContactData extends Component {
     state = {
@@ -204,4 +205,12 @@ class ContactData extends Component {
     }
 }
 
-export default ContactData;
+
+const mapStateToProps = state => {
+    return {
+        ingredients: state.ingredients,
+        price: state.totalPrice
+    }
+}
+
+export default connect(mapStateToProps)(ContactData);
