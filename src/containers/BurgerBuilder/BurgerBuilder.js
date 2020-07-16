85c53f26bd9afa92bfddfb3be7a88b4b33305d0e
cailliaud <cailliaud.a@gmail.com>
2020-07-16 13:04:06 +0200

    FInalize test

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,7 +11,7 @@ import * as actions from "../../store/actions/index";
 import axios from "../../config/axiosOrders";
 
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
 
     state = {
         purchasing: false,
---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,36 @@
+import authReducer from './authReducer';
+
+import * as actionTypes from '../actions/actionTypes';
+
+describe('auth reducer', () => {
+    it('should return the initial state', () => {
+        expect(authReducer(undefined, {})).toEqual({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        })
+    });
+
+    it('should store token upon loading', () => {
+        expect(authReducer({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        }, {
+            type: actionTypes.AUTH_SUCCESS,
+            token: 'some-token',
+            userId: 'some-user-id'
+        })).toEqual({
+            token: 'some-token',
+            userId: 'some-user-id',
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        })
+    });
+
+});
